60119bc43b1b469b45d2ac3cb4ea254da6a074e4
AKASH YADAV <akashgudise21@gmail.com>
2021-04-22 09:42:17 +0530

    All

--- src/App.js
+++ src/App.js
@@ -1,23 +1,279 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import a1 from "./a1.PNG";
+import a2 from "./a2.PNG";
+import a3 from "./a3.PNG";
+import a4 from "./a4.PNG";
+import a5 from "./a5.PNG";
+import a6 from "./a6.PNG";
+import a7 from "./a7.PNG";
+import a8 from "./a8.PNG";
+import a9 from "./a9.PNG";
+import a10 from "./a10.PNG";
+import a11 from "./a11.PNG";
+import e from "./5.png";
+import f from "./6.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bc">
+      <div className="h">
+        <img className="s2" src={a11}></img>
+        <input className="s"></input>
+        <img className="s1" src={a10}></img>
+      </div>
+
+      <div className="fl">
+        <div className="fll">
+          <div className="box1">
+            <img className="b1" src={a1}></img>
+          </div>
+          <div className="box11">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>
+                No additional skills are required to convert image to HTML
+                file,using this tool.
+              </p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">Today</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a2}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills </p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">2 days ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a3}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">3 days ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="fl">
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a4}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">4 days ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a5}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">5 days ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a6}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">6 days ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a7}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">1 week ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+      
+
+      <div className="fl">
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a8}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">1 week ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box2">
+            <img className="b2" src={a7}></img>
+          </div>
+          <div className="box21">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>No additional skills</p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">1 week ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="fll">
+          <div className="box1">
+            <img className="b1" src={a9}></img>
+          </div>
+          <div className="box11">
+            <div class="doughnut">
+              <div class="d1"></div>
+              <p className="sn">Library Update</p>
+            </div>
+
+            <div className="card">
+              <h4>Leveraging social proof for growth</h4>
+              <p>
+                No additional skills are required to convert image to HTML
+                file,using this tool.
+              </p>
+              <div className="f1">
+                <img className="ct" src={e}></img>
+                <p className="sn">Anna</p>
+                <img className="ct1" src={f}></img>
+                <p className="sn">1 week ago</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+
+
     </div>
   );
 }
